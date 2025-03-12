6c1e232464201ce4d48d4e306491b3fd6c14d511
rahul-j-eq <rahul.jani-t@equestsolutions.net>
2025-03-12 19:26:15 +0530

    Tour scene setup.

---
+++ Awesome Places/Controllers/TourViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TourViewController.swift
+//  Awesome Places
+//
+//  Created by Gaurav Patel on 12/03/25.
+//
+
+import UIKit
+
+class TourViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
